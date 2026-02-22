baca34bd5f232f7ef3f591a87ef4b79cf00239b1
Hasantha Medagedara <hasanthadilshanmedagedara@gmail.com>
2026-02-22 10:55:47 +0530

    fixed config error

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', //
-};
-
-export default nextConfig;
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
