b966b735c6b1a15478f21c68dcb75ba6b8380df4
Hasantha Medagedara <hasanthadilshanmedagedara@gmail.com>
2026-02-22 10:45:51 +0530

    aded export

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', //
+};
+
+export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     unoptimized: true,
   },
