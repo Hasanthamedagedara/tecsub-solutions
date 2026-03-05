27e503996069857b9bcd3e5467cff048419b207e
Hasantha Medagedara <hasanthadilshanmedagedara@gmail.com>
2026-03-05 16:09:06 +0530

    remove nav bar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -88,7 +88,7 @@ export default function Navbar() {
                 initial={{ y: -100 }}
                 animate={{ y: 0 }}
                 transition={{ duration: 0.8, ease: [0.25, 0.46, 0.45, 0.94] }}
-                className="fixed top-0 left-0 right-0 z-50 glass-nav"
+                className="hidden lg:block fixed top-0 left-0 right-0 z-50 glass-nav"
             >
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div className="flex items-center justify-between h-16 sm:h-20">
