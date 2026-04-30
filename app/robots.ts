4b0e4e24e2b434102b673318ac3a0be4e71037da
Hasantha Medagedara <hasanthadilshanmedagedara@gmail.com>
2026-04-30 16:17:46 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
                 disallow: ["/admin", "/api/"],
             },
         ],
-        sitemap: "https://tecsubsolutions.com/sitemap.xml",
+        sitemap: "https://tecsub.online/sitemap.xml",
     };
 }
