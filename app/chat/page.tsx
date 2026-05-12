ab6d76424794dfaf79dbc39aed91e6420da72c85
Hasantha Medagedara <hasanthadilshanmedagedara@gmail.com>
2026-05-12 11:46:59 +0530

    ap i update

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -60,7 +60,7 @@ export default function AIChatPage() {
         
         if (mode === "chat") {
             try {
-                const apiKey = "AIzaSyCmsbLxXLZwp3MOWpEaO0wcs9VnZYmugKQ";
+                const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
                 const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`, {
                     method: "POST",
                     headers: {
